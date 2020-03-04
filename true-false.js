3220509fd315e0e3f7286ed323f8ca730ade3e76
Jakaira-biswas <contactjakaria95@gmail.com>
2020-03-04 20:00:49 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//false 
+// 0
+// null 
+// NaN
+// false
+// ""
+
+
+//True 
+// " "
+// "0"
+// {}
+//[]
+
+
+const name = {};
+if(name){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
