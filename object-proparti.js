9946d104a3bbf52e9940820d64b25f75150012dc
Jakaira-biswas <contactjakaria95@gmail.com>
2020-03-04 22:35:02 +0600

    someting lear object releated

---
+++ object-proparti.js
@@ -0,0 +1,8 @@
+const student = [
+    {id:101, name:"Nasim"},
+    {id:102, name:"Sabbir"},
+    {id:103, name:"Alok"},
+    {id:104, name:"Jakaria"}
+];
+const result = student.map(s => s.id);
+console.log(result);
