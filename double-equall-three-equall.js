c1a3086fac88f68fb90cb9d6085c354cdebf3fe5
Jakaira-biswas <contactjakaria95@gmail.com>
2020-03-04 21:22:52 +0600

    double-equall and three euall all maybe done

---
+++ double-equall-three-equall.js
@@ -0,0 +1,42 @@
+
+// always 1 value true of with compare 
+const first1 = true;
+const second1 = 1;
+
+if(first1 == second1){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+// always 0 value false of with compare
+const first2 = false;
+const second2 = 0;
+if(first2 == second2){
+    console.log("condition2 is true");
+}else{
+    console.log("condition2 is false");
+}
+
+
+// == this equal check your variable only value
+const first3 = 1;
+const second3 = 2;
+if(first3 == second3){
+    console.log("condition3 is true");
+}else{
+    console.log("condition3 is false");
+}
+
+
+
+
+/// === this equal is check your variable type and value
+const first4 = 10;
+const second4 = "10";
+if(first4 === second4){
+    console.log("condition4 is true");
+}else{
+    console.log("condition4 is false");
+}
--- null-vs-undifaind.js
+++ null-vs-undifaind.js
@@ -1,21 +1,21 @@
 // Undefined 
 
-// let  name;
-// console.log(name);
+let  name;
+console.log(name);
 
-// function add(num1,num2){
-//     console.log(num1 + num2);
-// }
-// console.log(add(5,5));
+function add(num1,num2){
+    console.log(num1 + num2);
+}
+console.log(add(5,5));
 
-//  const fun = undefined;
-//  console.log(fun);
+ const fun = undefined;
+ console.log(fun);
 
-// let student = {name:"Jakaria", phone :5545};
-// console.log(student.address);
+let student = {name:"Jakaria", phone :5545};
+console.log(student.address);
 
-// const number = [1,2,3,8];
-// console.log(number[4]);
+const number = [1,2,3,8];
+console.log(number[4]);
 
  function add(x,y){
     console.log(y)
