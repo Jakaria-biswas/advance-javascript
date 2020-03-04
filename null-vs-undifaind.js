d4e0f7fd719c7ec13de78696c4fa0bd653bd46e7
Jakaira-biswas <contactjakaria95@gmail.com>
2020-03-04 20:52:42 +0600

    new change

--- null-vs-undifaind.js
+++ null-vs-undifaind.js
@@ -1,19 +1,26 @@
-Undefined 
+// Undefined 
 
-let  name;
-console.log(name);
+// let  name;
+// console.log(name);
 
-function add(num1,num2){
-    console.log(num1 + num2);
+// function add(num1,num2){
+//     console.log(num1 + num2);
+// }
+// console.log(add(5,5));
+
+//  const fun = undefined;
+//  console.log(fun);
+
+// let student = {name:"Jakaria", phone :5545};
+// console.log(student.address);
+
+// const number = [1,2,3,8];
+// console.log(number[4]);
+
+ function add(x,y){
+    console.log(y)
 }
-console.log(add(5,5));
+console.log(add(21));
 
- const fun = undefined;
- console.log(fun);
 
-let student = {name:"Jakaria", phone :5545};
-console.log(student.address);
-
-const number = [1,2,3,8];
-console.log(number[4]);
 
